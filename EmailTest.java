eb508f3342f5e6c3672ef9550f3903407a92d881
Shahin0710 <80635594+Shahin0710@users.noreply.github.com>
2021-03-26 12:34:32 +0600

    Add files via upload

---
+++ EmailTest.java
@@ -0,0 +1,14 @@
+package EmailApplication;
+
+public class EmailTest {
+
+public static void main(String[] args) {
+    
+		Email em1 = new Email("Md", "Shahin");
+		System.out.println(em1.showInfo());
+                
+                
+
+	}
+
+}
